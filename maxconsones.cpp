d2b4a8ff3a3cefb2359929559387fa926270d280
Srishti <srishtisingh910@gmail.com>
2025-08-11 21:39:46 +0530

    maximum consecutive ones

---
+++ maxconsones.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int maxones(vector<int>arr){
+    int n=arr.size();
+    int cnt=0,maxi=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==1){
+            cnt++;
+            maxi=max(maxi,cnt);
+        }
+        else{
+            cnt=0;
+        }
+    }
+    return cnt;
+}
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"max consecutive ones is "<<maxones(arr);
+    return 0;
+
+}
