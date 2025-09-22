238a2cda732264f770b7040f7cb3c5d828757acb
Srishti <srishtisingh910@gmail.com>
2025-09-22 14:58:23 +0530

    FOUR SUM

--- foursum.cpp
+++ foursum.cpp
@@ -1,17 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Problem: 4-Sum
+--------------
+Given an array nums of n integers and a target value,
+find all unique quadruplets (a, b, c, d) such that:
+    a + b + c + d = target
+
+Constraints:
+- No duplicate quadruplets
+- Order inside quadruplet doesn’t matter
+*/
+
 vector<vector<int>> fourSum(vector<int>& nums, int target) {
     int n = nums.size();
-    sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());  // sort to handle duplicates easily
     vector<vector<int>> ans;
 
+    // Fix the first two numbers (i and j)
     for (int i = 0; i < n; i++) {
         if (i > 0 && nums[i] == nums[i - 1]) continue;  // avoid duplicate i
 
         for (int j = i + 1; j < n; j++) {
             if (j > i + 1 && nums[j] == nums[j - 1]) continue;  // avoid duplicate j
 
+            // Two-pointer approach for remaining two numbers
             int k = j + 1;
             int l = n - 1;
 
@@ -23,12 +37,16 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
                     k++;
                     l--;
 
-                    // skip duplicates
+                    // skip duplicates for k and l
                     while (k < l && nums[k] == nums[k - 1]) k++;
                     while (k < l && nums[l] == nums[l + 1]) l--;
                 }
-                else if (sum > target) l--;
-                else k++;
+                else if (sum > target) {
+                    l--; // need smaller sum
+                }
+                else {
+                    k++; // need larger sum
+                }
             }
         }
     }
@@ -50,10 +68,8 @@ int main() {
     vector<vector<int>> result = fourSum(nums, target);
 
     cout << "Quadruplets with sum " << target << " are:\n";
-    for (int i = 0; i < result.size(); i++) {
-        for (int j = 0; j < result[i].size(); j++) {
-            cout << result[i][j] << " ";
-        }
+    for (auto &quad : result) {
+        for (int x : quad) cout << x << " ";
         cout << endl;
     }
 
