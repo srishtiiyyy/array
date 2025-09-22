5a1e8ca4d6080f223d5a7accaa70e45b4b03ab1c
Srishti <srishtisingh910@gmail.com>
2025-09-22 15:01:47 +0530

    INTERSECTION OF 2 SORTED ARRAYS

--- intersection.cpp
+++ intersection.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Problem: Intersection of Two Sorted Arrays
+-------------------------------------------
+Given two sorted arrays, find their intersection.
+Intersection means the common elements that appear in both arrays.
+
+Constraints:
+- Arrays are sorted
+- Result should contain unique elements (no duplicates)
+*/
+
 int main() {
     int n1, n2;
     cout << "Enter size of first array: ";
@@ -15,19 +26,20 @@ int main() {
     cout << "Enter elements of second array (sorted): ";
     for (int i = 0; i < n2; i++) cin >> nums2[i];
 
-    vector<int> ans;
+    vector<int> ans;  // store intersection
     int i = 0, j = 0;
 
-    // Find intersection
+    // Two-pointer traversal
     while (i < n1 && j < n2) {
         if (nums1[i] < nums2[j]) {
-            i++;
+            i++;  // move in nums1
         }
         else if (nums1[i] > nums2[j]) {
-            j++;
+            j++;  // move in nums2
         }
         else {
-            // Avoid duplicates in result
+            // nums1[i] == nums2[j]
+            // Add to result if not already added (avoid duplicates)
             if (ans.empty() || ans.back() != nums1[i]) {
                 ans.push_back(nums1[i]);
             }
