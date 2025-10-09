afbeef5b7e7fd79df8f284eedc6279cbae5e5346
Srishti <srishtisingh910@gmail.com>
2025-10-09 18:32:32 +0530

    REMOVE DUPLICATES 2

--- removeduplicates2.cpp
+++ removeduplicates2.cpp
@@ -19,7 +19,7 @@ public:
         int n = nums.size();
         if(n == 0) return 0;
 
-        int i = 0; // slow pointer
+        int i = 0; 
         for(int j = 1; j < n; j++) {
             if(nums[j] != nums[i]) {
                 i++;
