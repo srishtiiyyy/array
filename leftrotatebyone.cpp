95296973179ab3722107a5c4547734d513773be8
Srishti <srishtisingh910@gmail.com>
2026-04-02 11:44:43 +0530

    LEFT ROTATE BY ONE

--- leftrotatebyone.cpp
+++ leftrotatebyone.cpp
@@ -1,25 +1,94 @@
-#include<bits/stdc++.h>
-using namespace std;
-void rotate(vector<int> &arr){
-    int n=arr.size();
-    int a=arr[0];
-    for(int i=0;i<n-1;i++){
-        arr[i]=arr[i+1];
-    }
-    arr[n-1]=a;
-    for(int i=0;i<n;i++){
-        cout<<arr[i];
-    }
-}
-int main(){
-int n;
-cin>>n;
-vector<int>arr(n);
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-}
-cout<<"left rotate by one :";
-rotate(arr);
+/*
+=====================================================================
+ROTATE ARRAY BY ONE (LEFT ROTATION)
+=====================================================================
 
-return 0;
+Problem:
+--------
+Given an array, rotate it to the left by one position.
+
+---------------------------------------------------------------------
+
+Example:
+--------
+Input:
+nums = [1,2,3,4,5]
+
+Output:
+[2,3,4,5,1]
+
+---------------------------------------------------------------------
+
+Company Tags:
+-------------
+Walmart, MongoDB, Roche, Roblox, Seagate Technology,
+Bungie, Goldman Sachs, Deloitte, OYO Rooms, Byju's,
+Rakuten, Visa, Texas Instruments, Etsy, HCL Technologies,
+Epic Systems, NVIDIA, Reddit, DoorDash, Teladoc Health,
+PayPal, Red Hat, Zoho, Robinhood, Micron Technology,
+TCS, Cognizant, Accenture, Infosys, Capgemini, Wipro
+
+=====================================================================
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+=====================================================================
+APPROACH (IN-PLACE SHIFT)
+=====================================================================
+
+Time Complexity:
+----------------
+O(n)
+
+Space Complexity:
+-----------------
+O(1)
+
+*/
+void rotateByOne(vector<int> &nums)
+{
+    int n = nums.size();
+
+    // Store first element
+    int temp = nums[0];
+
+    // Shift all elements left
+    for (int i = 1; i < n; i++)
+    {
+        nums[i - 1] = nums[i];
+    }
+
+    // Place first element at end
+    nums[n - 1] = temp;
+}
+
+/*
+=====================================================================
+MAIN FUNCTION
+=====================================================================
+*/
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter array:\n";
+    for (int i = 0; i < n; i++)
+        cin >> nums[i];
+
+    rotateByOne(nums);
+
+    cout << "\nRotated Array:\n";
+    for (int x : nums)
+        cout << x << " ";
+
+    return 0;
 }
